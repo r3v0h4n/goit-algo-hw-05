066ecb270c7ebcbddbfd45905e6a784003f3ae8e
kachuros <Stanislav.Kachurovskyi@partenaires.videotron.com>
2024-07-20 15:47:17 +0300

    task2

--- task2/main.py
+++ task2/main.py
@@ -1,6 +1,36 @@
 
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    iterations = 0
+    upper_bound = None
+    
+    while left <= right:
+        iterations += 1
+        mid = left + (right - left) // 2
+        
+        if arr[mid] == target:
+            upper_bound = arr[mid]
+            return (iterations, upper_bound)
+        
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            upper_bound = arr[mid]
+            right = mid - 1
+    
+    if upper_bound is None and left < len(arr):
+        upper_bound = arr[left]
+    
+    return (iterations, upper_bound)
+
+
+
 def main():
-    pass
+    arr = [0.1, 1.2, 2.3, 3.4, 4.5, 5.6]
+    target = 2.0
+    result = binary_search(arr, target)
+    print("Кількість ітерацій:", result[0])
+    print("Верхня межа:", result[1])
 
 
 if __name__ == "__main__":
